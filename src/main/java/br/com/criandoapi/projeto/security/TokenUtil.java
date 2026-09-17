81498c8d8beebe2bd3bec1f8affe3e0a34347650
braian <braianjunior666@gmail.com>
2026-09-16 22:18:02 -0300

    Inicio das classes de geracao de token

--- src/main/java/br/com/criandoapi/projeto/security/TokenUtil.java
+++ src/main/java/br/com/criandoapi/projeto/security/TokenUtil.java
@@ -1,12 +1,19 @@
 package br.com.criandoapi.projeto.security;
 
 import java.security.Key;
+import java.util.Collections;
 import java.util.Date;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
 import br.com.criandoapi.projeto.model.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 
 
 public class TokenUtil {
@@ -31,5 +38,32 @@ public class TokenUtil {
 	
 	private static boolean isExpirationValid(Date expiration) {
 		return expiration.after(new Date(System.currentTimeMillis()));
+	}
+	
+	private static boolean isEmissorValid(String emissor) {
+		return emissor.equals(EMISSOR);
+	}
+	
+	private static boolean isSubjectValid(String username) {
+		return username != null && username.length() > 0;
+	}
+	
+	public static Authentication validate(HttpServletRequest request) {
+		String token = request.getHeader(HEADER);
+		token = token.replace(PREFIX, "");
+		
+		Jws<Claims> jwsClaims = Jwts.parserBuilder().setSigningKey(SECRET_KEY.getBytes())
+									.build()
+									.parseClaimsJws(token);
+		
+		String username = jwsClaims.getBody().getSubject();
+		String issue = jwsClaims.getBody().getIssuer();
+		Date expira = jwsClaims.getBody().getExpiration();
+		
+		if(isSubjectValid(username) && isEmissorValid(issue) && isExpirationValid(expira)) {
+			return new UsernamePasswordAuthenticationToken(username, null, Collections.emptyList());
+		}
+		
+		return null;
 	}
 }
